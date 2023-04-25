1dde66e0702d3592d7fb8b2dca7aeedaac968c5d
hussainFallah <129972005+hussainFallah@users.noreply.github.com>
2023-04-25 21:09:50 +0300

    Delete back4app.js

--- back4app.js
+++
@@ -1,5 +0,0 @@
-// Initialize Parse
-  Parse.initialize("TQ9K0la99pM5Lq9NVwGNQFP8CfPZ02oxmmIdGHh3", "jhjeYXrHg9GVlviYA2iSi2g57grAAUYcdGX5ez14"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
-  Parse.serverURL = "https://parseapi.back4app.com/";
-
-
