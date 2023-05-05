9fc37e647b63a80934dad7357cd4c5e6227aa57e
hussainFallah <129972005+hussainFallah@users.noreply.github.com>
2023-05-05 17:48:19 +0300

    Delete carsSubmitter.js

--- carsSubmitter.js
+++
@@ -1,9 +0,0 @@
-const movingDiv = document.querySelector('#formContainer');
-function showForm() {
-  movingDiv.classList.add("visible");
-  movingDiv.classList.remove("outPage");
-}
-function hideForm() {
-  movingDiv.classList.add("outPage");
-  // movingDiv.classList.remove("visible")
-}
