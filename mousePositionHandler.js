75895d95ffdb1604482f51629e4cb6ec96c8e710
hussainFallah <129972005+hussainFallah@users.noreply.github.com>
2023-05-05 17:51:50 +0300

    Delete mousePositionHandler.js

--- mousePositionHandler.js
+++
@@ -1,9 +0,0 @@
-let mouseposition={x:0,y:0};
-
-document.addEventListener('mousemove', function(event) {
-    const x = event.clientX;
-    const y = event.clientY;
-    const panel = document.getElementById('panel');
-    mouseposition.x=x;
-    mouseposition.y=y;
-});
